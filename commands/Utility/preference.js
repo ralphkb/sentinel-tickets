908f5121d612e01d106d1ccd4c0f2031917ecac4
Ralph <143613948+ralphkb@users.noreply.github.com>
2024-07-06 09:30:12 +0300

    Move preference command reply into a configurable embed

--- commands/Utility/preference.js
+++ commands/Utility/preference.js
@@ -4,6 +4,7 @@ const yaml = require("yaml");
 const configFile = fs.readFileSync("./config.yml", "utf8");
 const config = yaml.parse(configFile);
 const { blacklistDB } = require("../../init.js");
+const { configEmbed } = require("../../utils/mainUtils.js");
 
 module.exports = {
   enabled: config.commands.preference.enabled,
@@ -47,8 +48,33 @@ module.exports = {
         (await blacklistDB.get(`userPreference-${id}`)) || {};
       userPreferences[action] = enabled;
       await blacklistDB.set(`userPreference-${id}`, userPreferences);
+
+      const defaultValues = {
+        color: "#2FF200",
+        title: "User Preference",
+        description: "You will now {result}receive **{action}** DMs.",
+        timestamp: true,
+        footer: {
+          text: `Preference of ${interaction.user.username}`,
+          iconURL: `${interaction.user.displayAvatarURL({ extension: "png", size: 1024 })}`,
+        },
+      };
+
+      const preferenceEmbed = await configEmbed(
+        "preferenceEmbed",
+        defaultValues,
+      );
+
+      if (preferenceEmbed.data && preferenceEmbed.data.description) {
+        preferenceEmbed.setDescription(
+          preferenceEmbed.data.description
+            .replace(/\{result\}/g, `${enabled ? "" : "no longer "}`)
+            .replace(/\{action\}/g, action),
+        );
+      }
+
       await interaction.editReply({
-        content: `You will now ${enabled ? "" : "no longer "}receive **${action}** DMs.`,
+        embeds: [preferenceEmbed],
         ephemeral: true,
       });
     }
