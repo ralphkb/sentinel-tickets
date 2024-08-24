e5e18baf51334731f4f729d6fd6c22d54c9b816f
Ralph <143613948+ralphkb@users.noreply.github.com>
2024-08-24 10:23:02 +0300

    Refactor close request into a function

--- commands/Tickets/closerequest.js
+++ commands/Tickets/closerequest.js
@@ -1,21 +1,11 @@
-const {
-  SlashCommandBuilder,
-  PermissionFlagsBits,
-  ButtonBuilder,
-  ButtonStyle,
-  ActionRowBuilder,
-} = require("discord.js");
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
 const fs = require("fs");
 const yaml = require("yaml");
 const configFile = fs.readFileSync("./config.yml", "utf8");
 const config = yaml.parse(configFile);
-const { ticketsDB, ticketCategories } = require("../../init.js");
-const {
-  sanitizeInput,
-  logMessage,
-  configEmbed,
-  getUser,
-} = require("../../utils/mainUtils.js");
+const { ticketsDB } = require("../../init.js");
+const { getUser } = require("../../utils/mainUtils.js");
+const { closeRequestTicket } = require("../../utils/ticketCloseRequest.js");
 
 module.exports = {
   enabled: config.commands.closerequest.enabled,
@@ -63,59 +53,6 @@ module.exports = {
     }
 
     await interaction.deferReply();
-    let ticketButton = await ticketsDB.get(`${interaction.channel.id}.button`);
-
-    const closeButton = new ButtonBuilder()
-      .setCustomId("closeTicket")
-      .setLabel(config.closeButton.label)
-      .setEmoji(config.closeButton.emoji)
-      .setStyle(ButtonStyle[config.closeButton.style]);
-
-    let row = new ActionRowBuilder().addComponents(closeButton);
-
-    const defaultValues = {
-      color: "#FF2400",
-      title: "Ticket Close Request",
-      description:
-        "**{user} ({user.tag})** has requested to have his ticket closed.",
-      timestamp: true,
-      footer: {
-        text: `${interaction.user.tag}`,
-        iconURL: `${interaction.user.displayAvatarURL({ extension: "png", size: 1024 })}`,
-      },
-    };
-
-    const closeRequestEmbed = await configEmbed(
-      "closeRequestEmbed",
-      defaultValues,
-    );
-
-    if (closeRequestEmbed.data && closeRequestEmbed.data.description) {
-      closeRequestEmbed.setDescription(
-        closeRequestEmbed.data.description
-          .replace(/\{user\}/g, `${interaction.user}`)
-          .replace(/\{user\.tag\}/g, sanitizeInput(interaction.user.tag)),
-      );
-    }
-
-    let requestReply = {
-      embeds: [closeRequestEmbed],
-      components: [row],
-    };
-
-    if (config.commands.closerequest.pingRoles) {
-      const category = ticketCategories[ticketButton];
-      const pingRoles = category.ping_role_ids.length > 0;
-      const rolesToMention = pingRoles
-        ? category.ping_role_ids.map((roleId) => `<@&${roleId}>`).join(" ")
-        : "";
-      requestReply.content = rolesToMention;
-    }
-
-    await interaction.editReply(requestReply);
-
-    logMessage(
-      `${interaction.user.tag} requested his ticket #${interaction.channel.name} to be closed by staff.`,
-    );
+    await closeRequestTicket(interaction);
   },
 };
---
+++ utils/ticketCloseRequest.js
@@ -0,0 +1,68 @@
+const { ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
+const fs = require("fs");
+const yaml = require("yaml");
+const configFile = fs.readFileSync("./config.yml", "utf8");
+const config = yaml.parse(configFile);
+const { ticketsDB, ticketCategories } = require("../init.js");
+const { configEmbed, sanitizeInput, logMessage } = require("./mainUtils.js");
+
+async function closeRequestTicket(interaction) {
+  const ticketButton = await ticketsDB.get(`${interaction.channel.id}.button`);
+
+  const closeButton = new ButtonBuilder()
+    .setCustomId("closeTicket")
+    .setLabel(config.closeButton.label)
+    .setEmoji(config.closeButton.emoji)
+    .setStyle(ButtonStyle[config.closeButton.style]);
+
+  const row = new ActionRowBuilder().addComponents(closeButton);
+
+  const defaultValues = {
+    color: "#FF2400",
+    title: "Ticket Close Request",
+    description:
+      "**{user} ({user.tag})** has requested to have his ticket closed.",
+    timestamp: true,
+    footer: {
+      text: `${interaction.user.tag}`,
+      iconURL: `${interaction.user.displayAvatarURL({ extension: "png", size: 1024 })}`,
+    },
+  };
+
+  const closeRequestEmbed = await configEmbed(
+    "closeRequestEmbed",
+    defaultValues,
+  );
+
+  if (closeRequestEmbed.data && closeRequestEmbed.data.description) {
+    closeRequestEmbed.setDescription(
+      closeRequestEmbed.data.description
+        .replace(/\{user\}/g, `${interaction.user}`)
+        .replace(/\{user\.tag\}/g, sanitizeInput(interaction.user.tag)),
+    );
+  }
+
+  let requestReply = {
+    embeds: [closeRequestEmbed],
+    components: [row],
+  };
+
+  if (config.commands.closerequest.pingRoles) {
+    const category = ticketCategories[ticketButton];
+    const pingRoles = category.ping_role_ids.length > 0;
+    const rolesToMention = pingRoles
+      ? category.ping_role_ids.map((roleId) => `<@&${roleId}>`).join(" ")
+      : "";
+    requestReply.content = rolesToMention;
+  }
+
+  await interaction.editReply(requestReply);
+
+  logMessage(
+    `${interaction.user.tag} requested his ticket #${interaction.channel.name} to be closed by staff.`,
+  );
+}
+
+module.exports = {
+  closeRequestTicket,
+};
